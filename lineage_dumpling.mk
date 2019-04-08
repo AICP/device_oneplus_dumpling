b6f3c4ee1c16d1ebf9516a37451ffc4d3a4547c3
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    dumpling: Adding GSI keys
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- lineage_dumpling.mk
+++ lineage_dumpling.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/oneplus/dumpling/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 PRODUCT_NAME := lineage_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
