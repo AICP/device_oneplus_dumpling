LineageOS/android_device_oneplus_dumpling

dumpling: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_dumpling.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_dumpling-user \
+    lineage_dumpling-userdebug \
+    lineage_dumpling-eng

AICP/device_oneplus_dumpling

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_dumpling.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_dumpling-user \
+    aicp_dumpling-userdebug \
+    aicp_dumpling-eng