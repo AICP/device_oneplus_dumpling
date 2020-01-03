95cecac8d94f541d2569f5b8d7545ebf96d575a4
Carlos Solano <csolano@gmail.com>
2020-01-03 13:10:37 +0100

    dumpling: enable wireguard updater script
    Change-Id: I1ac7974ba24606f04fe10143bb771e5343c3bbf4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,5 +34,8 @@ TARGET_OTA_ASSERT_DEVICE := dumpling,OnePlus5T,oneplus5t
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Script launch on breakfast
+AICP_EXEC_PRE_SCRIPT := true
+
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
